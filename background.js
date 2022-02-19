8f36639ecf891d22e22e0b5e236e0321351ef648
SUGYO <pnr759@naver.com>
2022-02-20 04:12:51 +0900

    vanila js study

---
+++ background.js
@@ -0,0 +1,12 @@
+const images = [
+    "0.jpg", "1.jpg", "2.jpg"];
+
+    const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+    const image = document.createElement("img");
+
+    image.src = `img/${chosenImage}`;
+
+    console.log(image);
+
+    document.body.appendChild(image);
